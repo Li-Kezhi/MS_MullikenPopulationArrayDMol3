eb0e00102ef2509e0d142bbb3ee45cd0236272a2
daliwa7 <daliwa7@sina.com>
2016-07-26 12:40:39 +0800

    sorting resolved

--- MS_Mulliken.py
+++ MS_Mulliken.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import time      # TEST
+import sys   
 
-filename = "E:\\temp\\For python\\Mulliken\\SO2.outmol"
+position = "F:\\BaiduYunDownload\\test\\"
+filename = position + "SO2.outmol"
 
 def sequenceGenerator():                # Generate number sequence
     output = 0
@@ -33,7 +36,71 @@ class Electron:
         return self.occupation
     def get_num(self):
         return self.num
+    def __lt__(self, other):
+        if self.get_atomNum() < other.get_atomNum():
+            return True
+        elif self.get_atomNum() == other.get_atomNum():
+            if self.get_electron_n() < other.get_electron_n():
+                return True
+            elif self.get_electron_n() == other.get_electron_n():
+                if self.get_electron_l() < other.get_electron_l():
+                    return True
+                elif self.get_electron_l() == other.get_electron_l():
+                    if self.get_electron_ms() < other.get_electron_ms():
+                        return True
+                    elif self.get_electron_ms() == other.get_electron_ms():
+                        if self.get_num() < other.get_num():
+                            return True
+        return False
+    def __repr__(self):
+        return "Atom %i: n = %i, l = %i, ms = %i" %(self.atomNum, self.electron_n, self.electron_l, self.electron_ms)
+    def __str__(self):
+        return "Atom %i: n = %i, l = %i, ms = %i" %(self.atomNum, self.electron_n, self.electron_l, self.electron_ms)
+    def change_num(self, newnum):
+        self.num = newnum
 
+def exchangeSymMatrix(matrix, i, j):
+    """
+    Exchange the i'th and j'th row and column of a symmatrix matrix
+    """
+    matrix[[i, j], :] = matrix[[j, i], :]
+    matrix[:, [i, j]] = matrix[:, [j, i]]
+
+def sub_sort(electrons,matrix,low,high):
+    key = electrons[low]
+    while low < high:
+        while low < high and electrons[high] > key:
+            high -= 1
+        while low < high and electrons[high] < key:
+            electrons[low] = electrons[high]
+            exchangeSymMatrix(matrix, low, high)
+            low += 1
+            electrons[high] = electrons[low]
+            exchangeSymMatrix(matrix, low, high)
+    electrons[low] = key
+    return low
+
+def quick_sort(electrons,matrix,low = None,high = None):
+    sys.setrecursionlimit(100000)
+    if low == None:
+        low = 0
+    if high == None:
+        high = len(electrons) - 1
+    if low < high:
+        key_index = sub_sort(electrons,matrix,low,high)
+        quick_sort(electrons,matrix,low,key_index)
+        quick_sort(electrons,matrix,key_index+1,high)
+    
+def reIndex(electrons):
+    """
+    Re-index the electrons after sorting
+    """
+    naturalNumber = sequenceGenerator()
+    for electron in electrons:
+        electron.change_num(naturalNumber.next())
+
+
+clock0 = time.time()           # TEST
 
 # Step1: generate an array
 MullikenFlag = False
@@ -44,7 +111,7 @@ for line in open(filename):
     num += 1   # TEST
     #print(str(num))   # TEST
     if MullikenFlag == False:           # Start flag
-        if "Population analysis for representation" in line:
+        if len(line) == 52 and ("Population analysis for representation" in line):
             MullikenFlag = True
             continue
     else:
@@ -86,7 +153,25 @@ MullikenPopulationArray += mirror
 for i in xrange(len(listLikeArray)):
     MullikenPopulationArray[i,i] /= 2
 
+clock1 = time.time()    # TEST
+print(clock1-clock0)
+
+print(listElectrons)        # TEST
+quick_sort(listElectrons, MullikenPopulationArray)                                # Sort the result
+reIndex(listElectrons)
+clock2 = time.time()        # TEST
+print(clock2-clock1)
+print(listElectrons)        # TEST
+
 # Step 2: plotting
 plt.imshow(MullikenPopulationArray, interpolation = "nearest", cmap = "bone")
 plt.colorbar()
 plt.show()
+
+# Step 3: print electrons list
+outfile = open(position + "ElectronsList.txt", "w")
+num = 0
+for electron in listElectrons:
+    outfile.write("%7i  --  " % num + str(electron) + "\n")
+    num += 1
+outfile.close()
